78f9cae4457c949a07d8fbe5e981125a38d80269
JoshGaviola <martin100906@gmail.com>
2026-05-18 03:10:36 -0700

    Revert "Fix basePath for GitHub Pages (use /pathviz)"
    This reverts commit 9a76fc16414383f27f2a1bfea54cb336b406e0fd.

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   // Export the site as a fully static site (next export)
   output: 'export',
-  // If you will host at https://<user>.github.io/pathviz set basePath/assetPrefix
-  // Adjust these if your repo name differs (use lowercase to match repo name)
-  basePath: '/pathviz',
-  assetPrefix: '/pathviz/',
+  // If you will host at https://<user>.github.io/PathViz set basePath/assetPrefix
+  // Adjust these if your repo name differs
+  basePath: '/PathViz',
+  assetPrefix: '/PathViz/',
 };
 
 module.exports = nextConfig;
