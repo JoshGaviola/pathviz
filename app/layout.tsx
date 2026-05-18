9ed547d23a9d44025cf0b235cfd3d4d1e07f26d4
JoshGaviola <martin100906@gmail.com>
2026-05-18 08:25:56 -0700

    Add vercel speed insight for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col overflow-hidden">
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
